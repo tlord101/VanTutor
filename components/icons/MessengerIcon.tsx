8876bfe00e0c144f51a9b1b469baeaf5c236e802
David Oweilayefa <kook28515@gmail.com>
2025-11-01 00:02:57 +0100

    feat(icons): Update Messenger icon to green lightning bolt

--- components/icons/MessengerIcon.tsx
+++ components/icons/MessengerIcon.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
 export const MessengerIcon: React.FC<{ className?: string }> = ({ className = 'w-6 h-6' }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a2 2 0 01-2-2V7a2 2 0 012-2h6l2-2h2l-2 2z" />
-    <path strokeLinecap="round" strokeLinejoin="round" d="M7 16H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1" />
-  </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24">
+        {/* Green outline of the speech bubble */}
+        <path 
+            d="M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" 
+            stroke="#65a30d" 
+            strokeWidth="2" 
+            strokeLinecap="round" 
+            strokeLinejoin="round" 
+        />
+        {/* Green lightning bolt in the middle */}
+        <path 
+            d="M14.5 7.5l-5 5h3l-5 5 7-6h-3l5-4z" 
+            fill="#65a30d" 
+        />
+    </svg>
 );
